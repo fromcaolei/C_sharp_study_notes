bf2ae725c245fa6e8d63ef345da38214b9d71bf5
CaoLei <fromcaolei@foxmail.com>
2017-06-17 11:40:11 +0800

    学习.NET程序集的使用

---
+++ Windows_Components/Shapes/Shapes/Shapes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle
+    {
+        double Radius;
+
+        public Circle()
+        {
+            Radius = 0;
+        }
+
+        public Circle(double givenRadius)
+        {
+            Radius = givenRadius;
+        }
+
+        public double Area()
+        { 
+            return System.Math.PI * (Radius * Radius);
+        }
+    }
+
+    public class Triangle
+    {
+        double Base, Height;
+
+        public Triangle()
+        {
+            Base = 0;
+            Height = 0;
+        }
+
+        public Triangle(double givenBase, double givenHeight)
+        {
+            Base = givenBase;
+            Height = givenHeight;
+        }
+
+        public double Area()
+        {
+            return 0.5F * Base * Height;
+        }
+    }
+}
