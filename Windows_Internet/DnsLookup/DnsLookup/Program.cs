07529d90ce75dd20756aed324aed171f4307ef2a
CaoLei <fromcaolei@foxmail.com>
2017-06-21 17:08:52 +0800

    开始学习网络部分：DNS、WebClient。跳过学习XML文档说明章节

---
+++ Windows_Internet/DnsLookup/DnsLookup/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace DnsLookup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: DnsLookup hostname/IP Address");
+                Console.ReadLine();
+                return;
+            }
+
+            IPHostEntry ipHostEntry = Dns.GetHostEntry(args[0]);  //获得主机列表
+
+            Console.WriteLine("Host: {0}", ipHostEntry.HostName);
+
+            if (ipHostEntry.Aliases.Length > 0)  //获取关联的别名列表
+            {
+                Console.WriteLine("\nAliases");
+                foreach (string alias in ipHostEntry.Aliases)
+                {
+                    Console.WriteLine(alias);
+                }
+            }
+
+            Console.WriteLine("\nAddress(es):");
+            foreach (IPAddress address in ipHostEntry.AddressList)  //获取关联的IP地址列表
+            {
+                Console.WriteLine("Address: {0}", address.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Windows_Internet/WebClientDemo/WebClientDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace WebClientDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            client.BaseAddress = "http://www.microsoft.com";
+            string data = client.DownloadString("Office");  //请求http://www.microsoft.com/Office页面，返回的数据在string中
+            Console.WriteLine(data);
+            Console.ReadKey();
+        }
+    }
+}
