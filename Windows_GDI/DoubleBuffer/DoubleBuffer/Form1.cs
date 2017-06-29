60cf8bd3b288263b5f8ea747ada3babd0ffccc50
CaoLei <fromcaolei@foxmail.com>
2017-06-29 11:42:30 +0800

    学习GDI+的：在图像中创建钢笔（即修改Pen对象的填充物）、用图像绘制文本、双倍缓冲，它们的简单方法

---
+++ Windows_GDI/DoubleBuffer/DoubleBuffer/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DoubleBuffer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.FillRectangle(Brushes.White, ClientRectangle);  //将工作区域修改为白色
+
+            Random r = new Random();
+
+            //双倍缓冲用到的代码
+            Image i = new Bitmap(ClientRectangle.Width, ClientRectangle.Height);
+            Graphics dg = Graphics.FromImage(i);  //不应使用new，FromImage是一个静态方法，而不是一个类型
+
+            for (int x = 0; x < ClientRectangle.Width; x++)
+            {
+                for (int y = 0; y < ClientRectangle.Height; y += 10)
+                {
+                    Color c = Color.FromArgb(r.Next(255), r.Next(255), r.Next(255));
+                    using (Pen p = new Pen(c, 1))
+                    {
+                        //低性能原代码
+                        //g.DrawLine(p, new Point(0, 0), new Point(x, y));  ///////////////////
+
+                        //双倍缓冲用到的代码
+                        dg.DrawLine(p, new Point(0, 0), new Point(x, y));  ////////////////////
+                    }
+                }
+            }
+
+            //双倍缓冲用到的代码
+            g.DrawImage(i, ClientRectangle);  /////////////////////
+            i.Dispose();
+        }
+    }
+}
--- Windows_GDI/DrawingLines/DrawingLines/Form1.cs
+++ Windows_GDI/DrawingLines/DrawingLines/Form1.cs
@@ -11,14 +11,24 @@ namespace DrawingLines
 {
     public partial class Form1 : Form
     {
+        //在图像中创建钢笔用到
+        private Image theImage;
+        private Image smallImage;
+
         public Form1()
         {
             InitializeComponent();
+            SetStyle(ControlStyles.Opaque, true);
+
+            //在图像中创建钢笔用到
+            theImage = new Bitmap("../../../taylor.jpg");
+            smallImage = new Bitmap(theImage, new Size(theImage.Width / 10, theImage.Height / 10));
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             Graphics g = e.Graphics;
+            g.FillRectangle(Brushes.White, ClientRectangle);  //将工作区域修改为白色
 
             using (Pen blackPen = new Pen(Color.Black, 1))
             {
@@ -30,6 +40,21 @@ namespace DrawingLines
                     }
                 }
             }
+
+            //在图像中创建钢笔
+            Brush tBrush = new TextureBrush(smallImage, new Rectangle(0, 0, smallImage.Width, smallImage.Height));
+            Pen tPen = new Pen(tBrush, 40);
+            g.DrawRectangle(tPen, 0, 0, ClientRectangle.Width, ClientRectangle.Height);
+            //g.FillRectangle(tBrush, ClientRectangle);  //描绘Draw不同于填充Fill，无法使用Brush对象进行描绘，只能使用Pen对象
+            tPen.Dispose();
+            tBrush.Dispose();
+
+            //用图像绘制文本
+            Brush mBrush = new TextureBrush(smallImage, new Rectangle(0, 0, smallImage.Width, smallImage.Height));
+            Font trFont = new Font("Times New Roman", 16, FontStyle.Bold | FontStyle.Italic);
+            g.DrawString("Taylor Swift", trFont, mBrush, new Rectangle(30, 200, 160, trFont.Height));
+            mBrush.Dispose();
+            trFont.Dispose();
         }
     }
 }
